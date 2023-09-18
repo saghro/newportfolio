c5b41f8df2389543f94e0375df1c663e51a73cdb
saghro <ayoubsaghro27@gmail.com>
2023-09-18 16:56:30 +0100

    add desc

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,8 @@
 import "../../App.css";
 import DownloadIcon from "@mui/icons-material/Download";
+import logo from '../../assets/images/19362653.jpg'; // Assurez-vous d'avoir l'extension de fichier correcte (jpg)
 // import LearningCode from "../../assets/images/learn-coding-clipart.svg";
-import * as LottiePlayer from "@lottiefiles/lottie-player";
+
 
 export default function About() {
   const getDate = () => {
@@ -15,23 +16,22 @@ export default function About() {
   return (
     <div
       id="about"
-      className="min-h-screen bg-white flex flex-col text-center gap-5 text-black text-lg  font-normal"
+      className="min-h-screen bg-white flex flex-col text-center gap-5 text-black text-lg font-normal"
     >
       <div className="head text-5xl mt-12 font-bold" data-aos={"slide-down"}>
         About Me
       </div>
-      <div className="flex flex-row  gap-6 ml-8">
+      <div className="flex flex-row gap-6 ml-8">
         <div className="flex-col mt-14 flex flex-auto w-64 gap-6">
           <h3 className="text-3xl font-medium" data-aos={"fade-left"}>
-            I'm <span className="text-yellow-600">Ayoub SAGHRO,</span> a Web & Mobile 
-            developer
+            I'm <span className="text-yellow-600">Ayoub SAGHRO,</span> a Web & Mobile developer
           </h3>
           <p
             className="pt-5 leading-7 text-slate-900 text-justify"
             data-aos={"fade-left"}
           >
             I hold a bachelor's degree in Mathematics and Computer Sciences from FP Ouarzazate. I am {getDate()}{" "}
-            years old.I love exploring new technologies.
+            years old. I love exploring new technologies.
           </p>
           <p
             className="text-justify leading-7 text-slate-900"
@@ -41,38 +41,24 @@ export default function About() {
             I have a background as a developer with experience in technologies such as ReactJS,
             Next.js, TypeScript, Java, Spring, JEE, and more. Additionally,
             I am actively seeking opportunities to contribute to open-source projects
-
           </p>
         </div>
 
         <div
-          class="relative flex-auto w-32   sm:rounded-lg  pl-4"
+          className="relative flex-auto w-32 sm:rounded-lg pl-4"
           data-aos={"slide-left"}
         >
-          {/* <img
-            src={LearningCode}
-            alt="Learning Code"
-            className="motion-safe:animate-zoomy"
-          /> */}
-          <div className="motion-safe:animate-zoomy">
-            <lottie-player
-              autoplay
-              loop
-              mode="bounce"
-              src="https://assets10.lottiefiles.com/packages/lf20_w98qte06.json"
-              style={{ width: "350px" }}
-            />
-          </div>
-          <button className="py-3 mt-5 text-white mx-auto px-9 bg-blue-600 border-2 w-fit border-blue-500 rounded-3xl  hover:-translate-y-1.5 duration-[350ms] hover:duration-[350ms] hover:bg-blue-800 hover:scale-[1.023] focus:bg-blue-800 animate-zoomy">
-            <a
-              href={require("../../assets/files/CV-AYOUB-SAGHRO.pdf")}
-              download={"CV-AYOUB-SAGHRO.pdf"}
-            >
-              Download CV
-              <DownloadIcon />
-            </a>
-          </button>
+          <img src={logo} alt="Logo" style={{ width: "350px" }} />
         </div>
+        <button className="py-3 mt-5 text-white mx-auto px-9 bg-blue-600 border-2 w-fit border-blue-500 rounded-3xl hover:-translate-y-1.5 duration-[350ms] hover:duration-[350ms] hover:bg-blue-800 hover:scale-[1.023] focus:bg-blue-800 animate-zoomy">
+          <a
+            href={require("../../assets/files/CV-AYOUB-SAGHRO.pdf")}
+            download={"CV-AYOUB-SAGHRO.pdf"}
+          >
+            Download CV
+            <DownloadIcon />
+          </a>
+        </button>
       </div>
     </div>
   );
